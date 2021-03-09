5939ab0d78be3a3c83f7569e11a904da40da6f51
Patrik Morell <morellpatrik@gmail.com>
2021-03-09 20:37:19 +0100

    Add plugins to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FullCalendarModule } from "@fullcalendar/angular";
 
-
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin,
+  timeGridPlugin,
+  listPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -19,6 +30,7 @@ import { ScheduleComponent } from './schedule/schedule.component';
   ],
   imports: [
     BrowserModule,
+    FullCalendarModule,
     AppRoutingModule,
     NgbModule,
   ],
